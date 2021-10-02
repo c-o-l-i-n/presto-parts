271dac44ff7d76f6e4786e3b6903b6afa8efc23d
Colin Williams <williamsca20@gmail.com>
2021-10-02 14:51:21 -0400

    👾 Created boilerplate Electron app

---
+++ main.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+createWindow = () => {
+	const win = new BrowserWindow({
+		width: 800,
+		height: 600,
+		webPreferences: {
+			nodeIntegration: true,
+		},
+	})
+
+	win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+	createWindow()
+})
