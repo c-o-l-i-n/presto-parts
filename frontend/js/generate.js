65a5211658ad198b397d535656ce445818b59601
Colin Williams <williamsca20@gmail.com>
2021-10-14 20:49:51 -0400

    🧠 Adding change I forgot to add to last commit

--- frontend/js/generate.js
+++ frontend/js/generate.js
@@ -5,13 +5,6 @@ const songFoldersLocationElement = document.getElementById(
 )
 const songListElement = document.getElementById('song-list')
 const partsListElement = document.getElementById('parts-list')
-const submitButtonElement = document.getElementById('submit-button')
-const loaderElement = document.getElementById('loader')
-const notificationElement = document.getElementById('notification')
-const notificationTextElement = document.getElementById('notification-text')
-const closeNotificationButtonElement = document.getElementById(
-	'close-notification-btn'
-)
 
 // send data to backend on submit button click
 submitButtonElement.addEventListener('click', (e) => {
