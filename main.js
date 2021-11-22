be8047665a88cf94776b706aa902b81de7b3cffa
Colin Williams <williamsca20@gmail.com>
2021-11-22 15:15:14 -0500

    🗑 Removed all Python "backend" code

--- main.js
+++ main.js
@@ -1,43 +1,13 @@
 const { app, dialog, BrowserWindow, ipcMain } = require('electron')
-const { separateSongParts } = require('./js-backend/separateSongParts')
+const { separateSongParts } = require('./backend/separateSongParts')
 const {
 	generateInstrumentPartsAndMaster,
-} = require('./js-backend/generateInstrumentPartsAndMaster')
+} = require('./backend/generateInstrumentPartsAndMaster')
 
-let backend
 let mainWindow
 
 const showMessageBox = (type, message) => {
 	dialog.showMessageBox(mainWindow, { type: type, message: message })
-}
-
-const startBackend = (window, debugMode) => {
-	if (debugMode) {
-		// requires Python virtual environment called "env"
-		backend = require('child_process').execFile('./backend/env/bin/python', [
-			'./backend/backend.py',
-		])
-		backend.stdout.on('data', (data) => {
-			console.log('backend: ', data.toString('utf-8'))
-		})
-		backend.stderr.on('data', (data) => {
-			console.log(`backend error: ${data.toString('utf-8')}`)
-			// get the last line of the Python traceback after ': '
-			const errorMessage = data.toString('utf-8').match(/(?<=: )(.*)\n$/gm)[0]
-			showMessageBox('error', errorMessage)
-		})
-	} else {
-		backend = require('child_process').exec('./backend/dist/backend')
-		backend.stderr.on('data', (data) => {
-			// get the last line of the Python traceback after ': '
-			const errorMessage = data.toString('utf-8').match(/(?<=: )(.*)\n$/)[0]
-			showMessageBox('error', errorMessage)
-		})
-	}
-}
-
-const terminateBackend = () => {
-	backend.kill()
 }
 
 createWindow = () => {
@@ -59,10 +29,7 @@ createWindow = () => {
 
 app.whenReady().then(() => {
 	createWindow()
-	startBackend(mainWindow, (debugMode = false))
 })
-
-app.on('before-quit', terminateBackend)
 
 ipcMain.on('show-message-box', (e, type, message) => {
 	showMessageBox(type, message)
