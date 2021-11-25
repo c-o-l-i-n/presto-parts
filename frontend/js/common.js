61d5d5e1b227d5b0cd146c405dd4276c548afa1e
Colin Williams <williamsca20@gmail.com>
2021-11-24 22:20:45 -0500

    ❌ Removed unused axios package

--- frontend/js/common.js
+++ frontend/js/common.js
@@ -1,26 +1,9 @@
-const axios = require('axios')
 const { ipcRenderer } = require('electron')
 
 // DOM Elelements
 const mainElement = document.getElementById('main')
 const submitButtonElement = document.getElementById('submit-button')
 const loaderElement = document.getElementById('loader')
-
-// send data to backend
-const submitForm = (endpoint, data) => {
-	loaderElement.classList.add('is-active')
-	axios
-		.post('http://127.0.0.1:14161/' + endpoint, data)
-		.then((response) => {
-			loaderElement.classList.remove('is-active')
-			clearForm()
-			ipcRenderer.send('show-message-box', 'info', response.data)
-		})
-		.catch((error) => {
-			loaderElement.classList.remove('is-active')
-			console.log(error)
-		})
-}
 
 // show the loader screen overlay
 const showLoader = () => {
