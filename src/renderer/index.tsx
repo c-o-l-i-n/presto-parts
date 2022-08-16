b2e838973db5accbdda498a937a86ef04ab29187
Colin Williams <colin@colinaw.com>
2022-08-15 22:39:12 -0400

    ⚛️ #8 Create React and TS boilerplate

---
+++ src/renderer/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.scss'
+
+const el = document.getElementById('root')
+if (el === null) throw new Error('Root container missing in index.html')
+
+const root = ReactDOM.createRoot(el)
+root.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+)
---
+++ webpack.renderer.config.js
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const rules = require('./webpack.rules')
+const plugins = require('./webpack.plugins')
+
+rules.push({
+	test: /\.scss$/,
+	use: [
+		{ loader: 'style-loader' },
+		{ loader: 'css-loader' },
+		{ loader: 'sass-loader' },
+	],
+})
+
+module.exports = {
+	module: {
+		rules,
+	},
+	plugins: plugins,
+	resolve: {
+		extensions: ['.js', '.ts', '.jsx', '.tsx', '.css'],
+	},
+}
