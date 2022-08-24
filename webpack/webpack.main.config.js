3d3c1d3710a7df474ce59f8e5eb4177aea32ab5b
Colin Williams <colin@colinaw.com>
2022-08-23 22:38:25 -0400

    📦 #8 Move webpack config files to thier own folder

--- webpack/webpack.main.config.js
+++ webpack/webpack.main.config.js
@@ -1,11 +1,10 @@
 module.exports = {
 	/**
-	 * This is the main entry point for your application, it's the first file
+	 * This is the main entry point for the application, it's the first file
 	 * that runs in the main process.
 	 */
 	entry: './src/main/main.ts',
 	target: 'electron-main',
-	// Put your normal webpack config below here
 	module: {
 		rules: require('./webpack.rules'),
 	},
---
+++ webpack/webpack.renderer.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	module: {
+		rules: require('./webpack.rules'),
+	},
+	plugins: require('./webpack.plugins'),
+	resolve: {
+		extensions: ['.js', '.ts', '.jsx', '.tsx', '.css'],
+	},
+	devtool: 'source-map',
+}
